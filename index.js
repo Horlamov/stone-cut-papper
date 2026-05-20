832e75263ea574a9c02836e24bab20a508bd5ebf
Denis H <XoMA32@yandex.ru>
2026-05-20 21:51:04 +0300

    Цикл, вариативность

--- index.js
+++ index.js
@@ -2,37 +2,43 @@ let userChoice;
 let computerChoice;
 let isWinner = false;
 
-userChoice = prompt('Введите: "камень", "ножницы" или "бумага"');
-console.log("Ты выбрал: ", userChoice);
-
-let randomNum = Math.floor(Math.random() * 3);
-console.log(randomNum);
-
-if (randomNum === 0) {
-    computerChoice = "камень"
-} else if (randomNum === 1) {
-    computerChoice = "ножницы"
-} else {
-    computerChoice = "бумага"
-}
-
-console.log(computerChoice);
+while(isWinner === false) {
 
 
-if (userChoice === computerChoice) {
-    alert("Ничья! Играем дальше")
-} else if (userChoice === "камень" && computerChoice === "ножницы" ||
+    userChoice = prompt('Введите: "камень", "ножницы" или "бумага"').toLowerCase();
+    console.log("Ты выбрал: ", userChoice);
+
+    let randomNum = Math.floor(Math.random() * 3);
+    console.log(randomNum);
+    alert("Твой выбор: " + userChoice);
+
+    if (randomNum === 0) {
+        computerChoice = "камень"
+    } else if (randomNum === 1) {
+        computerChoice = "ножницы"
+    } else {
+        computerChoice = "бумага"
+    }
+
+    console.log(computerChoice);
+
+    if (userChoice === "камень" || userChoice === "ножницы" || userChoice === "бумага") {
+        alert("Компьютер выбрал: " + computerChoice);
+
+        if (userChoice === computerChoice) {
+            alert("Ничья! Играем дальше")
+        } else if (userChoice === "камень" && computerChoice === "ножницы" ||
             userChoice === "ножницы" && computerChoice === "бумага" ||
             userChoice === "бумага" && computerChoice === "камень") {
-    isWinner = true;
-    alert("Ты победил!")
+            isWinner = true;
+            alert("Ты победил!")
 
-} else {
-    alert("Компьютер выиграл")
+        } else {
+            alert("Компьютер выиграл")
+        }
+
+    } else {
+
+        alert("Введите корректное значение: камень, ножницы или бумага");
+    }
 }
-
-// if (userChoice === "камень" && computerChoice === "ножницы" ||
-//     userChoice === "ножницы" && computerChoice === "бумага" ||
-//     userChoice === "бумага" && computerChoice === "камень") {
-//     isWinner = true;
-// } else {console.log()}
